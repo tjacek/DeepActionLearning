f4337b34984bd8c8c10383274e2c89525283d4ff
tjacek <tjacek@student.agh.edu.pl>
2015-09-01 11:55:12 +0200

    sgd optimisation

--- spatial_nn/simple_convnet.lua
+++ spatial_nn/simple_convnet.lua
@@ -48,11 +48,20 @@ function train(model,dataset,labels,hyper_params)
 
   epoch = epoch or 1
   local time = sys.clock()
-  for t = 1,dataset:size()[1] do
-    local inputs = torch.Tensor(1,1,geometry[1],geometry[2])
-    local targets = torch.Tensor(1)
-    inputs[1]=dataset[t]
-    targets[1]=labels[t]
+  local nsamples=dataset:size()[1]
+  for t = 1,nsamples,hyper_params.batchSize do
+    local inputs = torch.Tensor(hyper_params.batchSize,1,geometry[1],geometry[2])
+    local targets = torch.Tensor(hyper_params.batchSize)
+    local k = 1
+    for i = t,math.min(t+hyper_params.batchSize-1,nsamples) do
+      local sample = dataset[i]
+      local input = sample:clone()
+      --local target= torch.Tensor(hyper_params.batchSize)
+      inputs[k] = input
+      targets[k] = labels[i]
+      k = k + 1
+    end
+
     local feval = function(x)
       collectgarbage()
       if x ~= parameters then
@@ -62,32 +71,31 @@ function train(model,dataset,labels,hyper_params)
       gradParameters:zero()
 
          -- evaluate function for complete mini batch
+      print(inputs:size())
       local outputs = model:forward(inputs)
       local f = criterion:forward(outputs, targets)
 
       local df_do = criterion:backward(outputs, targets)
       model:backward(inputs, df_do)
 
-      confusion:add(outputs[1], targets[1])
+      for i = 1,hyper_params.batchSize do
+        confusion:add(outputs[i], targets[i])
+      end
       return f,gradParameters
     end
 
-    lbfgsState = {}--lbfgsState or {
-    --        maxIter = hyper_params.maxIter,
-    --        lineSearch = hyper_params.lswolfe
-    --}
-    print(lbfgsState.maxIter)
-    optim.lbfgs(feval, parameters, lbfgsState)
+    sgd_optimisation(feval, parameters,hyper_params)   
 
-    print('LBFGS step')
+    print('SGD step')
     print(' - progress in batch: ' .. t .. '/' .. dataset:size()[1])
-    print(' - nb of iterations: ' .. lbfgsState.nIter)
-    print(' - nb of function evalutions: ' .. lbfgsState.funcEval)
+    --print(' - nb of iterations: ' .. lbfgsState.nIter)
+   -- print(' - nb of function evalutions: ' .. lbfgsState.funcEval)
   end
 
   time = sys.clock() - time
   time = time / dataset:size()[1]
   print("<trainer> time to learn 1 sample = " .. (time*1000) .. 'ms')
+  
 
    -- print confusion matrix
   print(confusion)
@@ -95,6 +103,24 @@ function train(model,dataset,labels,hyper_params)
   confusion:zero()
 
   epoch = epoch + 1
+end
+
+function lbfgs_optimisation(feval, parameters,hyper_params)
+  lbfgsState = {}--lbfgsState or {
+    --        maxIter = hyper_params.maxIter,
+    --        lineSearch = hyper_params.lswolfe
+    --}
+    --print(lbfgsState.maxIter)
+  optim.lbfgs(feval, parameters, lbfgsState)
+end
+
+function sgd_optimisation(feval, parameters,hyper_params)
+  sgdState = sgdState or {
+            learningRate = hyper_params.learningRate,
+            momentum = hyper_params.momentum,
+            learningRateDecay = 5e-7
+         }
+  optim.sgd(feval, parameters, sgdState)
 end
 
 function test(model,dataset,labels,hyper_params)
@@ -125,6 +151,9 @@ function default_hyper_params()
   hyper_params={}
   hyper_params.maxIter=100
   hyper_params.lswolfe=true
+  hyper_params.learningRate= 0.05
+  hyper_params.momentum=0
+  hyper_params.batchSize=1
   return hyper_params
 end
 
