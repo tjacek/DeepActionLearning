3a16b991993355cb10fbfcc0d86c60842d850a75
tjacek <tjacek@student.agh.edu.pl>
2015-09-07 11:49:27 +0200

    refactoring projection

--- preproc/projection.lua
+++ preproc/projection.lua
@@ -1,7 +1,7 @@
 require 'torch' 
 require 'math'
  
-function projection_zx(action)
+function projection(action,plane)
   local max_z=torch.max(action)
   local min_z=first_nonzero(action)
   local size=action:size()
@@ -9,16 +9,25 @@ function projection_zx(action)
   for i=1,size[1] do
     for j=1,size[2] do
       for k=1,size[3] do
-        local value=math.ceil(action[i][j][k],1)
-        if( value>2) then
-          action_zx[i][j][value-min_z+1]=k
-          --action_zx[i][j][value-1]=k
-          --action_zx[i][j][value-2]=k
-        end
+        plane(i,j,k,min_z,action)
       end
     end
   end
   return action_zx
+end
+
+function plane_zx(i,j,k,min_z,action)
+  local value=math.ceil(action[i][j][k],1)
+  if( value>2) then
+    action_zx[i][j][value-min_z+1]=k
+  end
+end
+
+function plane_zy(i,j,k,min_z,action)
+  local value=math.ceil(action[i][j][k],1)
+  if( value>2) then
+    action_zx[i][value-min_z+1][k]=j
+  end
 end
 
 function first_nonzero(action)
@@ -39,17 +48,12 @@ end
 
 function projected_img(input_file,output_file)
   input=torch.load(input_file)
-  output=projection_zx(input)
+  output=projection_zy(input)
   torch.save(output_file,output)
 end
-
---function round(num, idp)
---  local mult = 10^(idp or 0)
---  return math.floor(num * mult + 0.5) / mult
---end
 
 if table.getn(arg) > 1 then
   --input="/home/user/Desktop/dataset_1/train/"
   --output_data="/home/user/Desktop/dataset_1/train.tensor"
-  projected_img(arg[1],arg[2])
+  projected_img(arg[1],arg[2],plane_zx)
 end
