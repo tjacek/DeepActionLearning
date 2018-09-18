37c804009c6e79a90d92ae5c9f6d579c69d3ce33
tjacek <tjacek@student.agh.edu.pl>
2018-09-18 04:18:12 -0800

    dtw metric

---
+++ dtw/__init__.py
@@ -0,0 +1,24 @@
+import numpy as np
+
+def dtw_metric(s,t):
+    n=len(s)
+    m=len(t)
+    dwt=np.zeros((n+1,m+1),dtype=float)
+    for i in range(1,n+1):
+        dwt[i][0]=np.inf
+    for i in range(1,m+1):
+        dwt[0][i]=np.inf
+    dwt[0][0]=0.0
+    for i in range(1,n+1):
+        for j in range(1,m+1):
+            cost=d1(s[i-1],t[j-1])
+            dwt[i,j]=cost+min([dwt[i-1][j],dwt[i][j-1],dwt[i-1][j-1]])
+    return dwt[n][m]
+
+def d1(v,d):
+    return np.linalg.norm(v-d)
+
+def d2(v,u):
+    dist=np.dot(u,v)
+    dist/=np.linalg.norm(v) * np.linalg.norm(u)
+    return dist
