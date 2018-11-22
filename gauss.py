6a15eefd2d168d089868b1582e47db165736acbb
tjacek <tjacek@student.agh.edu.pl>
2018-11-22 01:41:28 +0100

    gauss sampling

--- gauss.py
+++ gauss.py
@@ -3,20 +3,26 @@ import numpy as np
 from sklearn.mixture import GaussianMixture
 import basic.extr
 
-def gausian_feats(ts):
+def gausian_feats(ts,max_comp=5):
 #    feat_i=np.expand_dims(feat_i,axis=0).T
     print(ts.shape)
-    mixture=GaussianMixture(n_components=3,
+
+    mixtures,bic_values=zip(*[fit_gaussian(ts,i+1) for i in range(max_comp)])
+    n_comp=np.argmin(bic_values)
+    bic_n=bic_values[n_comp]
+    print("n_components:%d bic:%.2f" % (n_comp,bic_n))
+    feats=[n_comp,bic_n]
+    return feats+prepare_feats(mixtures[-1])
+
+def fit_gaussian(ts,n):
+    mixture=GaussianMixture(n_components=n,
                    covariance_type="full", max_iter=20, random_state=0)
     samples=rejection_sampling(ts,size=500)
-    print(samples.shape)
     samples=np.expand_dims(samples,axis=0).T
     mixture.fit(samples)
-    means=np.squeeze(mixture.means_) 
-    covars=np.squeeze(mixture.covariances_)
-    return list(means) + list(covars)
+    return mixture,mixture.bic(samples)
 
-def rejection_sampling(feats,size=1000):
+def rejection_sampling(feats,size=10000):
     seq_len=feats.shape[0]
     feats-=np.min(feats)
     feats/=np.max(feats)
@@ -26,6 +32,14 @@ def rejection_sampling(feats,size=1000):
     return np.array([ samples[i] for i in indexes
                         if( feats[i]>threshold[i])])
 
+def prepare_feats(mixture):
+    means=np.squeeze(mixture.means_) 
+    covars=np.squeeze(mixture.covariances_)
+    indexes=np.argsort(means,axis=0)
+    means=[ means[i] for i in indexes]
+    covars=[ covars[i] for i in covars]
+    return means + covars
+
 extract=basic.extr.Extractor(gausian_feats)
 
-extract( "../LSTM/norm_full", "test")
+extract( "../_LSTM/all", "gauss.txt")
