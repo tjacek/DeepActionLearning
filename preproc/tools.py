29a5b85a84897aa35665aebeacb8f9ea4c93d43e
tjacek <tjacek@student.agh.edu.pl>
2019-01-10 20:34:26 +0100

    normalization

--- preproc/tools.py
+++ preproc/tools.py
@@ -1,8 +1,9 @@
 import numpy as np
 import preproc,preproc.sampling,preproc.normal
 
-def for_ts_network(size=128):
-    upsample=preproc.sampling.SplineUpsampling(size)
-    all_funs=[upsample,preproc.normal.z_norm]
+def for_ts_network(size=128,norm=True):
+    all_funs=[preproc.sampling.SplineUpsampling(size)]
+    if(norm):
+        all_funs.append(preproc.normal.z_norm)
     pipeline=preproc.Pipeline(all_funs)
     return preproc.Preproc(pipeline)
