d5ef45d3d8d4b971ff52e17b4ac77edc852fe2c1
tjacek <tjacek@student.agh.edu.pl>
2019-01-15 15:33:25 +0100

    rewrite ensemmble extractor

--- deep/extr.py
+++ deep/extr.py
@@ -2,18 +2,13 @@ import numpy as np
 import deep.reader,utils
 import basic.extr,deep.preproc
 
-class EnsembleExtr(object):
-    def __init__(self,fun):
-        self.fun=fun
-
-    def __call__(in_path,out_path):
-        all_paths=utils.bottom_dirs(in_path)
-        new_paths=utils.switch_paths(out_path,all_paths)
-        for in_i,out_i in zip(all_paths,new_paths):
-            self.fun(in_i,out_i):
-
-def ensemble_frame_extractor(nn_path,cat_feat=True,n_frames=4):
-    return EnsembleExtr(build_frame_extractor(nn_path,cat_feat,n_frames))
+def ensemble_extractor(in_path,nn_path,out_path):
+    model_paths=utils.bottom_dirs(in_path)
+    seqs_paths=utils.switch_paths(out_path,model_paths)
+    utils.make_dir(out_path)
+    for model_path_i,seq_path_i in zip(model_paths,seqs_paths):
+        extr_i=build_frame_extractor(model_path_i,cat_feat=True,n_frames=4)
+        extr_i(in_path,out_path_i)
 
 def build_ts_extractor(nn_path):
     nn_reader=deep.reader.NNReader()
