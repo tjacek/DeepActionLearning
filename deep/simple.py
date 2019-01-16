397fac678df5398abba244e60fe07dc04d35f5f9
tjacek <tjacek@student.agh.edu.pl>
2019-01-16 23:58:20 +0100

    remove simple

--- deep/simple.py
+++
@@ -1,9 +0,0 @@
-def build_model(params):
-    print(params)
-    input_shape=params["input_shape"]
-    n_filters=params["num_filters"]
-    filter_size2D=params["filter_size"]
-    pool_size2D=params["pool_size"]
-    p_drop=params["p"]
-    n_cats=params['n_cats']
-    n_hidden=params.get('n_hidden',32) 
