35cdab3b448e9e42ebb9208b75cf29d8375ed04a
tjacek <tjacek@student.agh.edu.pl>
2019-01-17 23:57:56 +0100

    data agumentation

--- preproc/tools.py
+++ preproc/tools.py
@@ -1,5 +1,5 @@
 import numpy as np
-import preproc,preproc.sampling,preproc.normal
+import preproc,preproc.agum,preproc.sampling,preproc.normal
 
 def for_ts_network(size=128,norm=True):
     all_funs=[preproc.sampling.SplineUpsampling(size)]
@@ -7,3 +7,7 @@ def for_ts_network(size=128,norm=True):
         all_funs.append(preproc.normal.z_norm)
     pipeline=preproc.Pipeline(all_funs)
     return preproc.Preproc(pipeline)
+
+def basic_agum(in_path,out_path):
+    agum_upsampl=preproc.agum.Agumentation([preproc.agum.SamplingAgum()])#,Scale()])
+    agum_upsampl(in_path,out_path)
