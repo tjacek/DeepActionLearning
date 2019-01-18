91d4e4b7544fce82b8a5d4768e1e1181e20e4037
tjacek <tjacek@student.agh.edu.pl>
2019-01-18 11:41:07 +0100

    concat files

--- seq/tools.py
+++ seq/tools.py
@@ -1,22 +1,33 @@
+import os
 import numpy as np
-import seq.io
+import seq.io,utils
 
-def concat(in_path1='seqs/max_z',in_path2='seqs/all',out_path='seqs/full'):
+def concat_files(paths,out_path='seqs/full'):
     read_actions=seq.io.build_action_reader(img_seq=False,as_dict=True)
-    actions1,actions2=read_actions(in_path1),read_actions(in_path2)
-    names=actions1.keys()
-    unified_actions=[ unify_actions(actions1[name_i],actions2[name_i],dim=1) 
-                        for name_i in names]
+    if(os.path.isdir(paths)):
+        paths=utils.bottom_dirs(paths)
+    action_sets=[read_actions(path_i) for path_i in paths]
+    concated_actions=concat_actions(action_sets)
     save_actions=seq.io.ActionWriter(img_seq=False)
-    save_actions(unified_actions,out_path)
+    save_actions(concated_actions,out_path)
+
+def concat_actions(action_sets):
+    concated_actions=action_sets[0]
+    if(len(action_sets)==1):
+        return concated_actions
+    for action_i in action_sets[1:]:
+        concated_actions=pair_concat(concated_actions,action_i)
+    return concated_actions
+
+def pair_concat(actions1,actions2):
+    names=actions2.keys()
+    return { name_i:unify_actions(actions1[name_i],actions2[name_i],dim=1) 
+                for name_i in names}
 
 def unify_actions(action1,action2,dim=1):
     array1,array2=action1.as_array(),action2.as_array()
     if(array1.shape[0]!=array2.shape[0]):
         new_dim=min(array1.shape[0],array2.shape[0])
         array1,array2=array1[:new_dim],array2[:new_dim]
-    print(array1.shape)
-    print(array2.shape)
     new_array=np.concatenate((array1,array2),axis=dim)
-    print(new_array.shape)
     return action1.clone(new_array)
