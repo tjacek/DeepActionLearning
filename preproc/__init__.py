ae7a14a18385062127a58416d37bdfb5df808d84
tjacek <tjacek@student.agh.edu.pl>
2019-01-19 13:10:14 +0100

    preproc dict

--- preproc/__init__.py
+++ preproc/__init__.py
@@ -1,22 +1,26 @@
 import numpy as np
-import seq.io
+import seq.io,seq.tools
 
 class Preproc(object):
     def __init__(self,preproc_fun=None):
-        if(not preproc_fun):
-            preproc_fun=z_norm
         self.preproc_fun=preproc_fun
-        
-    def __call__(self,in_path,out_path):
-    	read_actions=seq.io.build_action_reader(img_seq=False,as_dict=False)
-        actions=read_actions(in_path)
+            
+    def __call__(self,actions):
         def action_helper(action_i):
             feats=[ self.preproc_fun(feat_i) 
                         for feat_i in action_i.as_features()]
             img_seq=np.array(feats).T
             return action_i.clone(img_seq)
-        actions=[action_helper(action_i) 
-                        for action_i in actions]
+        if(type(actions)==dict):
+            return { action_i.name:action_helper(action_i) 
+                        for action_i in actions.values()}
+        return  [action_helper(action_i) 
+                    for action_i in actions]
+
+    def save(self,in_path,out_path):
+        read_actions=seq.io.build_action_reader(img_seq=False,as_dict=False)
+        actions=read_actions(in_path)
+        actions=self.transform(actions)
         save_actions=seq.io.ActionWriter(False)
         save_actions(actions,out_path)
 
--- preproc/tools.py
+++ preproc/tools.py
@@ -1,7 +1,8 @@
 import numpy as np
 import preproc,preproc.agum,preproc.sampling,preproc.normal
+import seq.io,seq.tools
 
-def for_ts_network(size=128,norm=True):
+def for_ts_network(norm=True,size=128):
     all_funs=[preproc.sampling.SplineUpsampling(size)]
     if(norm):
         all_funs.append(preproc.normal.z_norm)
@@ -11,3 +12,12 @@ def for_ts_network(size=128,norm=True):
 def basic_agum(in_path,out_path):
     agum_upsampl=preproc.agum.Agumentation([preproc.agum.SamplingAgum()])#,Scale()])
     agum_upsampl(in_path,out_path)
+
+def preproc_concat(paths,out_path,norms,preproc_build=None):
+    read_actions=seq.io.build_action_reader(img_seq=False,as_dict=True)
+    action_sets=[read_actions(path_i) for path_i in paths]
+    action_sets=[preproc_build(norm_i)(action_i) 
+                    for action_i,norm_i in zip(action_sets,norms)]
+    concated_actions=seq.tools.concat_actions(action_sets)
+    save_actions=seq.io.ActionWriter(img_seq=False)
+    save_actions(concated_actions,out_path)
