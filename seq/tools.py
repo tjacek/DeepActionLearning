8c42f514364f14493cd356b21c3c14a45671f0cb
tjacek <tjacek@student.agh.edu.pl>
2019-01-21 18:16:00 +0100

    seq type

--- seq/tools.py
+++ seq/tools.py
@@ -3,12 +3,13 @@ import numpy as np
 import seq.io,utils
 
 def concat_files(paths,out_path='seqs/full'):
-    read_actions=seq.io.build_action_reader(img_seq=False,as_dict=True)
     if(os.path.isdir(paths)):
         paths=utils.bottom_dirs(paths)
+    seq_type=get_seq_type(paths[0])
+    read_actions=seq.io.build_action_reader(img_seq=seq_type,as_dict=True)
     action_sets=[read_actions(path_i) for path_i in paths]
     concated_actions=concat_actions(action_sets)
-    save_actions=seq.io.ActionWriter(img_seq=False)
+    save_actions=seq.io.ActionWriter(img_seq=seq_type)
     save_actions(concated_actions,out_path)
 
 def concat_actions(action_sets):
@@ -31,3 +32,8 @@ def unify_actions(action1,action2,dim=1):
         array1,array2=array1[:new_dim],array2[:new_dim]
     new_array=np.concatenate((array1,array2),axis=dim)
     return action1.clone(new_array)
+
+def get_seq_type(in_path):
+    paths=utils.bottom_files(in_path)
+    postfix=paths[0].split(".")[-1]
+    return postfix=="png"
