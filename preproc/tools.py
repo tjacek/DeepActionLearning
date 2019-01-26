a96957c7aa9cb424f7b0110a7339cb17e4995679
tjacek <tjacek@student.agh.edu.pl>
2019-01-26 13:37:09 +0100

    refactore preproc_cat

--- preproc/tools.py
+++ preproc/tools.py
@@ -1,6 +1,7 @@
 import numpy as np
 import preproc,preproc.agum,preproc.sampling,preproc.normal
-import seq.io,seq.tools
+import seq.io,seq.tools,utils
+from sets import Set
 
 def for_ts_network(norm=True,size=128):
     all_funs=[preproc.sampling.SplineUpsampling(size)]
@@ -13,7 +14,8 @@ def basic_agum(in_path,out_path):
     agum_upsampl=preproc.agum.Agumentation([preproc.agum.SamplingAgum()])#,Scale()])
     agum_upsampl(in_path,out_path)
 
-def preproc_concat(paths,out_path,norms,preproc_build=None):
+def preproc_concat(in_path,out_path,restr=None,preproc_build=None):
+    paths,norms,preproc_build=prepare_concat(in_path,restr,preproc_build)
     read_actions=seq.io.build_action_reader(img_seq=False,as_dict=True)
     action_sets=[read_actions(path_i) for path_i in paths]
     action_sets=[preproc_build(norm_i)(action_i) 
@@ -21,3 +23,13 @@ def preproc_concat(paths,out_path,norms,preproc_build=None):
     concated_actions=seq.tools.concat_actions(action_sets)
     save_actions=seq.io.ActionWriter(img_seq=False)
     save_actions(concated_actions,out_path)
+
+def prepare_concat(in_path,restr=None,preproc_build=None):
+    paths=utils.top_dirs(in_path)
+    if(type(restr)!=list):
+        restr=[restr]
+    restr=Set(restr)
+    norms=[ not (path_i in restr) for path_i in paths]
+    if(not preproc_build):
+        preproc_build=for_ts_network
+    return paths,norms,preproc_build
