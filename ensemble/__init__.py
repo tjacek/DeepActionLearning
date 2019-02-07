1f92917e75465a7a23c277c0114de0429c5b1bc1
tjacek <tjacek@student.agh.edu.pl>
2019-02-07 12:56:51 +0100

    ensemble refactoring

--- ensemble/__init__.py
+++ ensemble/__init__.py
@@ -7,6 +7,13 @@ class Ensemble(object):
     def __init__(self,clf=None):
         self.clf=clf if(clf) else tools.logistic_cls
 
+    def __call__(self,handcrafted_path=None,deep_path=None,feats=(250,100)):
+        datasets=ensemble.data.get_datasets(handcrafted_path,deep_path,feats)
+        y_true,all_pred=self.get_prediction(datasets)
+        indiv=ensemble.inspect.cls_accuracy(y_true,all_pred,stats=False)
+        y_pred=vote(all_pred)
+        ensemble.tools.show_result(y_true,y_pred,datasets[0])
+
     def get_prediction(self,datasets):
         result=[ self.train_model(i,data_i) 
                     for i,data_i in enumerate(datasets)]
@@ -15,20 +22,12 @@ class Ensemble(object):
         return y_true,all_pred
 
     def train_model(self,i,dataset_i):
+        train,test=dataset_i.split()
         clf,clf_name=self.clf()
         print("dataset %d %s" % (i,clf_name))
-        train,test=dataset_i.split()
         clf = clf.fit(train.X, train.y)
         y_pred = clf.predict(test.X)
         return test.y,y_pred
-
-def learning(handcrafted_path=None,deep_path=None,feats=(250,100)):
-    datasets=ensemble.data.get_datasets(handcrafted_path,deep_path,feats)
-    ens=Ensemble(tools.SVC_cls)
-    y_true,all_pred=ens.get_prediction(datasets)
-    indiv=ensemble.inspect.cls_accuracy(y_true,all_pred,stats=False)
-    y_pred=vote(all_pred)
-    ensemble.tools.show_result(y_true,y_pred,datasets[0])
 
 def vote(all_votes):
     all_votes=np.array(all_votes)
