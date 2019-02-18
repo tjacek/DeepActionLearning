bde5b41756298608a5dd2ed8157667f9b9fc1bf2
tjacek <tjacek@student.agh.edu.pl>
2019-02-18 09:10:12 +0100

    fix compute_score

--- ensemble/tools.py
+++ ensemble/tools.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pandas as pd
 from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import accuracy_score,precision_score,recall_score,f1_score
+from sklearn.metrics import precision_recall_fscore_support     #accuracy_score,precision_score,recall_score,f1_score
 from sklearn.metrics import classification_report
 from sklearn.metrics import confusion_matrix
 import seaborn as sn
@@ -50,11 +50,8 @@ def show_errors(y_true,y_pred,names):
     print(error_descs)
 
 def compute_score(y_true,y_pred):
-    accuracy=accuracy_score(y_true,y_pred)
-    precision=precision_score(y_true,y_pred,average='micro')
-    recall=recall_score(y_true,y_pred,average='micro')
-    f1=f1_score(y_true,y_pred,average='micro')
-    return "%0.4f,%0.4f,%0.4f,%0.4f" % (accuracy,precision,recall,f1)
+    precision,recall,f1,support=precision_recall_fscore_support(y_true,y_pred,average='macro')
+    return "%0.4f,%0.4f,%0.4f,%0.4f" % (precision,recall,precision,f1)
 
 def heat_map(conf_matrix):
     dim=conf_matrix.shape
