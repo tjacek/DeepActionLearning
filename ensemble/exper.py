ea938b5587c88a0c13ce997fa6c65b256d16b7de
tjacek <tjacek@student.agh.edu.pl>
2019-02-19 09:17:29 +0100

    remove_sufix

--- ensemble/exper.py
+++ ensemble/exper.py
@@ -4,7 +4,7 @@ def simple_experiment(dir_path,common_paths,deep_paths,feats,ensemble,out_path='
         name_i=get_name(feature_set_i)
         print(name_i)
         full_path_i=get_full_paths(dir_path,feature_set_i)
-        line_i=ensemble(full_path_i,deep_paths,feats)
+        line_i=ensemble(full_path_i,deep_paths,feats,show=False)
         result+=name_i+','+line_i+'\n'
     out_file=open(out_path,'w+')
     out_file.write(result)
@@ -12,10 +12,11 @@ def simple_experiment(dir_path,common_paths,deep_paths,feats,ensemble,out_path='
 
 def get_name(feature_set_i):
     if(type(feature_set_i)==str):
-        return feature_set_i
+        return remove_sufix(feature_set_i)
     if(type(feature_set_i)==list):
+        feature_set_i=[ remove_sufix(name_i) for name_i in  feature_set_i]
         return '+'.join(feature_set_i)
-    return '-'
+    return "-"
 
 def get_full_paths(dir_path,feature_set_i):
     if(type(feature_set_i)==str):
@@ -24,4 +25,5 @@ def get_full_paths(dir_path,feature_set_i):
         return [dir_path+'/'+feature_i for feature_i in feature_set_i]
     return []
 
-
+def remove_sufix(name_i):
+    return name_i.split('.')[0]
