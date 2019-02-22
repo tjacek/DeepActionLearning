dbc11be2d4537e4687697f123525e06c0e522120
tjacek <tjacek@student.agh.edu.pl>
2019-02-22 13:05:06 +0100

    handle special case in experiment

--- ensemble/exper.py
+++ ensemble/exper.py
@@ -1,14 +1,23 @@
+import ensemble
+
+def dict_experiment(arg_dict,clf_type,out_path):
+    ens=ensemble.Ensemble(clf_type,False,None)
+    dir_path,common_paths,deep_paths,feats=arg_dict['dir'],arg_dict['common'],arg_dict['deep'],arg_dict['feats']
+    simple_experiment(dir_path,common_paths,deep_paths,feats,ens,out_path)
+
 def simple_experiment(dir_path,common_paths,deep_paths,feats,ensemble,out_path='result.csv'):
-    result=''
     desc=get_descritpion(deep_paths,feats)
-    for feature_set_i in common_paths:
+    def line_helper(feature_set_i):
+        if( not feature_set_i and not deep_paths):
+            return ''
         name_i=get_name(feature_set_i)
         print(name_i)
         full_path_i=get_full_paths(dir_path,feature_set_i)
         score_i,n_feats_i=ensemble(full_path_i,deep_paths,feats,show=False)
-        result+='%s,%s,%d,%s\n' % (name_i,desc,n_feats_i,score_i)
+        return '%s,%s,%d,%s' % (name_i,desc,n_feats_i,score_i)
+    lines=[line_helper(feature_set_i) for feature_set_i in common_paths]      
     out_file=open(out_path,'w+')
-    out_file.write(result)
+    out_file.write('\n'.join(lines))
     out_file.close()
 
 def get_descritpion(deep_paths,feats):
