4dbb63bfe396f5136fb23126c88a75c96693a7dd
tjacek <tjacek@student.agh.edu.pl>
2019-03-04 12:42:51 +0100

    detectors on training data

--- ensemble/outliner.py
+++ ensemble/outliner.py
@@ -1,13 +1,14 @@
 import sklearn.svm
 import ensemble.data
 
-def make_outliner_detection(handcrafted_path,deep_path,feats):
+def make_outliner_detectors(handcrafted_path,deep_path,feats):
     datasets,n_feats=ensemble.data.get_datasets(handcrafted_path,deep_path,feats)
     outliners_detections=[train_one_SVM(i,data_i) 
                             for i,data_i in enumerate(datasets)]
 
 def train_one_SVM(i,data_i):
     clf_i = sklearn.svm.OneClassSVM()
-    clf_i.fit(data_i.X)
-    print("Train one class SVM %d",i)
+    train_i,test_i=data_i.split()
+    clf_i.fit(train_i.X)
+    print("Train one class SVM %d" % i)
     return clf_i
