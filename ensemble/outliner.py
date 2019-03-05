2ebed911716bc8904a553ea8df28e3e2bcadfccb
tjacek <tjacek@student.agh.edu.pl>
2019-03-05 15:25:03 +0100

    save detectors

--- ensemble/outliner.py
+++ ensemble/outliner.py
@@ -1,10 +1,15 @@
 import sklearn.svm
-import ensemble.data
+import utils,ensemble.data
+from joblib import dump, load
 
-def make_outliner_detectors(handcrafted_path,deep_path,feats):
+def make_outliner_detectors(handcrafted_path,deep_path,feats,out_path):
     datasets,n_feats=ensemble.data.get_datasets(handcrafted_path,deep_path,feats)
-    outliners_detections=[train_one_SVM(i,data_i) 
+    outliner_detectors=[train_one_SVM(i,data_i) 
                             for i,data_i in enumerate(datasets)]
+    utils.make_dir(out_path)
+    for i,detector_i in enumerate(outliner_detectors):
+        path_i= '%s/feature_set_%d.joblib' % (out_path,i)
+        dump(detector_i, path_i) 
 
 def train_one_SVM(i,data_i):
     clf_i = sklearn.svm.OneClassSVM()
