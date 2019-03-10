cf4cf9fa447b986a62270fbf3ffaf981a1d270e8
tjacek <tjacek@student.agh.edu.pl>
2019-03-10 01:50:37 +0100

    correct weights

--- ensemble/outliner.py
+++ ensemble/outliner.py
@@ -1,3 +1,4 @@
+import numpy as np
 import sklearn.svm
 import utils,ensemble.data
 from joblib import dump, load
@@ -7,15 +8,23 @@ class OutlinerWeights(object):
         self.outliner_detectors = outliner_detectors
 
     def __call__(self,all_samples_i):
-        return [outliner_j.predict(sample_ij)[0] 
-                    for sample_ij, outliner_j in zip(all_samples_i,self.outliner_detectors)]
+        weights=np.array([outliner_j.predict(sample_ij)[0] 
+                    for sample_ij, outliner_j in zip(all_samples_i,self.outliner_detectors)])
+        weights[weights==(-1)]=0
+        return weights
 
-    def get_weights(self,dataset_i):
-        train,test=dataset_i.split()
-        insts=test.ordered_raw()
-        return {inst_i.name: self(inst_i.data) 
-                    for inst_i in insts}
-		
+    def get_weights(self,ensemble_dataset):
+        return { name_i: self(samples_i) 
+                    for name_i,samples_i in ensemble_dataset.items()}
+
+def get_weights(handcrafted_path,deep_path,feats,outliner_path):
+    datasets,n_feats=ensemble.data.get_datasets(handcrafted_path,deep_path,feats)
+    ensemble_dataset=ensemble.data.to_ensemble_samples(datasets)
+    outliner_weights=read_detectors(outliner_path)
+    weights=outliner_weights.get_weights(ensemble_dataset['test'])
+    zero_weights(weights)
+    return weights
+
 def make_outliner_detectors(handcrafted_path,deep_path,feats,out_path):
     datasets,n_feats=ensemble.data.get_datasets(handcrafted_path,deep_path,feats)
     outliner_detectors=[train_one_SVM(i,data_i) 
@@ -41,7 +50,7 @@ def read_detectors(in_path):
 def examine(handcrafted_path,deep_path,feats,detectors_path):
     detectors=read_detectors(detectors_path)
     datasets,n_feats=ensemble.data.get_datasets(handcrafted_path,deep_path,feats)    
-    ensemble_dataset=to_ensemble_samples(datasets)
+    ensemble_dataset=ensemble.data.to_ensemble_samples(datasets)
     def examine_helper(dict_i):
         weights_sum=0
         for name_j,samples_j in dict_i.items():
@@ -54,12 +63,12 @@ def examine(handcrafted_path,deep_path,feats,detectors_path):
     print("@@@@@@@@@@@@@@@@@@@@@@@@@@@@\nTest ")
     print(examine_helper(ensemble_dataset['test']))       
 
-def to_ensemble_samples(datasets):
-    train,test=datasets[0].to_instances().split()
-    ens_insts=[ dataset_i.to_instances() for dataset_i in datasets]
-    def ens_helper(names):
-    	return { name_j:[insts_i[name_j].data.reshape(1, -1) 
-    	            for insts_i in ens_insts]
-    	                for name_j in names} 
-    return {'train':ens_helper(train.names()),
-            'test':ens_helper(test.names())}
+def zero_weights(weights):
+    n_dims=weights.values()[0].shape[0]
+    zero_names=[name_i 
+                    for name_i,weights_i in weights.items()
+                        if(np.mean(weights_i)==0.0)]
+    print(len(zero_names))
+    for name_i in zero_names:
+        weights[name_i]= np.ones((n_dims),dtype=float)
+    return weights    	
