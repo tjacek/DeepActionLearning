79cfe9747b16a72e02892562699c4c01c40a2e84
tjacek <tjacek@student.agh.edu.pl>
2019-03-14 18:31:12 +0100

    save datasets

--- plot.py
+++ plot.py
@@ -4,6 +4,7 @@ import numpy as np
 import random,basic
 from sets import Set
 from sklearn.manifold import TSNE
+import utils
 
 class CatColors(object):
     def __init__(self,cats):
@@ -21,6 +22,19 @@ class PersonColors(object):
     
     def __call__(self,i,y_i):
         return float(self.persons[i]%2)
+
+def save_datasets(in_path,out_path):
+    utils.make_dir(out_path)
+    paths=utils.bottom_files(in_path)
+    for in_path_i in paths:
+        name_i=in_path_i.split('/')[-1]
+        out_path_i=out_path+'/'+name_i
+        dataset=basic.read_dataset(in_path_i)
+        X,y=dataset.X,dataset.y
+        color_helper=PersonColors(dataset.persons)
+        X=TSNE(n_components=2,perplexity=30).fit_transform(X)
+        plt_i=plot_embedding(X,y,title=name_i,color_helper=color_helper,show=False)
+        plt.savefig(out_path_i)
 
 def show_dataset(in_path,title="plot"):
     dataset=basic.read_dataset(in_path)
@@ -47,7 +61,7 @@ def plot_embedding(X,y,title="plot",color_helper=None,show=True):
     x_min, x_max = np.min(X, 0), np.max(X, 0)
     X = (X - x_min) / (x_max - x_min)
    
-    color_helper=color_helper if(color_helper) else lambda i:0
+    color_helper=color_helper if(color_helper) else lambda i,y_i:0
     plt.figure()
     ax = plt.subplot(111)
 
@@ -57,8 +71,9 @@ def plot_embedding(X,y,title="plot",color_helper=None,show=True):
                    color=plt.cm.tab20( color_i),
                    fontdict={'weight': 'bold', 'size': 9})
     print(x_min,x_max)
-    plt.xticks(np.arange(x_min, x_max, 0.005)), plt.yticks([])
+    #plt.xticks(np.arange(x_min, x_max, 0.005)), plt.yticks([])
     if title is not None:
         plt.title(title)
     if(show):
         plt.show()
+    return plt
