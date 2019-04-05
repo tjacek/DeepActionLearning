c62ffcc4bda303f23a43f72e936d93f20b358cb9
tjacek <tjacek@student.agh.edu.pl>
2019-04-05 22:19:11 +0200

    cluster module

--- ensemble/pick/cluster.py
+++ ensemble/pick/cluster.py
@@ -9,8 +9,16 @@ def cluster_quality(dataset):
     labels=np.array(train_data.y)
     cats=np.unique(labels)
     cls_labels=cluster(train_data)
-    for cat_i in cats:
+    cls_labels+=1
+    def cls_helper(cat_i):
     	indic_i=indicator_vector(cat_i,labels)
+        cls_indic=cls_labels*indic_i
+        counted_i=np.bincount(cls_indic)
+        counted_i[0]=0
+        in_cls=np.max(counted_i)
+        cat_size=np.sum(indic_i)
+        return float(in_cls)/float(cat_size)
+    return [ cls_helper(cat_i) for cat_i in cats]
 
 def cluster(dataset):
     n_clust=dataset.n_cats()
@@ -19,6 +27,6 @@ def cluster(dataset):
     return kmeans.labels_
 
 def indicator_vector(i,y):
-    ind_vector=np.zeros(y.shape)
+    ind_vector=np.zeros(y.shape,dtype=int)
     ind_vector[y==i]=1.0
     return ind_vector
