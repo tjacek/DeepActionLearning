04fa81c1bf365ab1cb69d14a8155829ce7df4091
tjacek <tjacek@student.agh.edu.pl>
2019-04-09 16:51:43 +0200

    train_ts_ensemble

--- deep/tools.py
+++ deep/tools.py
@@ -5,21 +5,39 @@ import basic.extr
 import deep.reader,deep.train,deep.preproc
 import deep.convnet,deep.autoconv,os
 
-class BinaryModels(object):
-    def __init__(self,as_dataset="cats",name="nn",n_frames=4):
-        self.n_frames=n_frames
-        self.name=name
-        self.as_dataset=as_dataset
+def train_ts_ensemble(in_path,out_path,num_iter=1500):
+    load_data=deep.preproc.LoadData("time_series")
+    all_in_paths=utils.bottom_dirs(in_path)
+    all_out_paths=utils.switch_paths(out_path,all_in_paths)
+    for in_path_i,out_path_i in zip(all_in_paths,all_out_paths):
+        X_train,y_train,X_test,y_test=load_data(in_path_i)
+        dim=X_train.shape[-1]
+        ts_network=deep.convnet.make_model(y_train,"time_series",dim=dim)
+        ts_network=deep.train.train_super_model(X_train,y_train,ts_network,num_iter=num_iter)
+        verify_model(y_test,X_test,ts_network)
+        ts_network.get_model().save(out_path_i)
+#class BinaryModels(object):
+#    def __init__(self,as_dataset="cats",name="nn",binary,n_frames=4):
+#        self.n_frames=n_frames
+#        self.name=name
+#        self.as_dataset=as_dataset
+#        self.binary=binary
 
-    def __call__(self,in_path,out_path,num_iter=10):
-        X,y,frame_preproc=deep.preproc.frame_dataset(in_path,
-                    as_dataset=self.as_dataset,n_frames=self.n_frames)
-        person_ids=np.unique(y)
-        model_paths=[out_path+'/'+self.name+str(i) for i in person_ids]
-        binary_datasets=[deep.preproc.binarize(y,person_i) 
-                            for person_i in person_ids]
-        for y_i,out_i in zip(binary_datasets,model_paths):
-            train_model(X,y_i,out_i,num_iter)                   
+#    def __call__(self,in_path,out_path,num_iter=10):
+#        X,y,frame_preproc=deep.preproc.frame_dataset(in_path,
+#                    as_dataset=self.as_dataset,n_frames=self.n_frames)        
+#        for y_i,out_i in zip(binary_datasets,model_paths):
+#            train_model(X,y_i,out_i,num_iter)                   
+
+#    def get_train_sets(self,y):
+#        unique_cats=np.unique(y)
+#        model_paths=[out_path+'/'+self.name+str(i) for i in person_ids]
+#        if(self.binary):
+#            binary_cats=[deep.preproc.binarize(y,cat_i) 
+#                            for cat_i in unique_cats]
+#            return zip(binary_cats,model_paths)
+#        else:
+#            return zip(y,model_paths)
 
 def multi_persons_model(in_path,out_path,num_iter=970,n_frames=4):
     X,y,frame_preproc=deep.preproc.frame_dataset(in_path,as_dataset="persons",n_frames=n_frames)
