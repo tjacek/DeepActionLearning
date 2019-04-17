6ce263da32720f5b6339a3f209d76c3c5dfb0666
tjacek <tjacek@student.agh.edu.pl>
2019-04-17 16:00:02 +0200

    plot by category

--- plot/action_img.py
+++ plot/action_img.py
@@ -1,13 +1,24 @@
 import numpy as np
-import seq.io,utils
+import seq,seq.io,utils
 import cv2
 
-def make_action_imgs(in_path,out_path):
+def make_action_imgs(in_path,out_path,by_cat=True):
     action_reader=seq.io.build_action_reader(img_seq=False,as_dict=False,as_group=False)
     actions=action_reader(in_path)
     print(len(actions))
     utils.make_dir(out_path)
-    for action_i in actions:
-        action_img_i=np.array(action_i.img_seq)
-        out_i=out_path+"/"+utils.set_prefix(action_i.name,".png")
-        cv2.imwrite(out_i,action_img_i)
+    if(by_cat):
+       actions=seq.by_cat(actions)
+       for cat_j,actions_j in actions.items():
+            cat_path=out_path+"/"+str(cat_j)
+       	    utils.make_dir(cat_path)
+       	    for action_ij in actions_j:
+                save_action_img(action_ij,cat_path)
+    else:
+        for action_i in actions:
+            save_action_img(action_i,out_path)
+
+def save_action_img(action_i,out_path):
+    action_img_i=np.array(action_i.img_seq)
+    out_i=out_path+"/"+utils.set_prefix(action_i.name,".png")
+    cv2.imwrite(out_i,action_img_i)
