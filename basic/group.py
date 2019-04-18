e30d4fefe1f96290881b7bfc50034994ecc80361
tjacek <tjacek@student.agh.edu.pl>
2019-04-18 20:14:56 +0200

    action img by cat

--- basic/group.py
+++ basic/group.py
@@ -5,9 +5,14 @@ class GroupFun(object):
         self.fun = fun
         self.dirs=dirs
 		
-    def __call__(self,nn_path,out_path):
+    def __call__(self,nn_path,out_path,other_args=None):
         model_paths=utils.bottom_dirs(nn_path) if(self.dirs) else utils.bottom_files(nn_path)
         seqs_paths=utils.switch_paths(out_path,model_paths)
         utils.make_dir(out_path)
         for model_path_i,seq_path_i in zip(model_paths,seqs_paths):
-            self.fun(model_path_i,seq_path_i)
+            if(other_args):
+                other_args["in_path"]=model_path_i
+                other_args["out_path"]=seq_path_i
+                self.fun(**other_args)
+            else:
+                self.fun(model_path_i,seq_path_i)
