84ba4e81aee5aa33ebc8e9f2f0c6873dc494a8df
tjacek <tjacek@student.agh.edu.pl>
2019-05-08 18:53:00 +0200

    selection

---
+++ ensemble/exper/selection.py
@@ -0,0 +1,26 @@
+import matplotlib.pyplot as plt
+import ensemble,ensemble.pick
+
+class SelectionExper(object):
+    def __init__(self,clf,dict_arg,detector_path,metric=None):
+        if(not metric):
+            self.metric=ensemble.pick.mean_cat
+        self.clf=clf
+        self.dict_arg=dict_arg
+        self.detector_path=detector_path
+    
+    def __call__(self,ens_arg,n_datasets=20):
+        x=range(n_datasets+1)[1:]
+        y=[self.single_exp(ens_arg,x_i) for x_i in x ]
+        plt.plot(x,y)
+        plt.show() 
+        return y
+
+    def single_exp(self,ens_arg,x_i):
+        ens_i=self.get_ensemble(x_i)
+        return ens_i(show=False,**ens_arg)[0][0]
+
+    def get_ensemble(self,n_cls):
+        quality=clf_quality(self.dict_arg,self.detector_path,quality_metric=self.metric)
+        allowed_list=ensemble.pick.selectors.sort_list(quality,n_cls)
+        return ensemble.Ensemble(clf,selector=allowed_list)
