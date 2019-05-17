9c451d3ae660ce1e01f54afa9ef18708215924ba
tjacek <tjacek@student.agh.edu.pl>
2019-05-17 13:53:10 +0200

    refactore ensemble_inspect

--- ensemble/inspect.py
+++ ensemble/inspect.py
@@ -2,17 +2,19 @@ import numpy as np
 from sklearn.metrics import accuracy_score
 import ensemble
 
-def ensemble_stast(arg_dicts,test_ensemble=None):
-    test_ensemble= test_ensemble if(test_ensemble) else ensemble.Ensemble()
-    common_paths,deep_paths,feats=arg_dicts['common'],arg_dicts['deep'],arg_dicts['feats']
-    datasets,n_feats=test_ensemble.get_datasets(common_paths,deep_paths,feats)
-    y_true,all_pred=test_ensemble.get_prediction(datasets)
+def ensemble_stas(arg_dicts,test_ensemble=None):
+    y_true,all_pred=get_preds(arg_dicts,test_ensemble)
     all_pred=indiv_votes(all_pred)
-    indiv_acc=individual_accuracy(y_true,all_pred)
+    indiv_acc=clf_accuracy(y_true,all_pred)
     print(indiv_acc)
     print("min %f median %f mean %f max %f" % basic_stats(indiv_acc))
 
-def individual_accuracy(y_true,all_pred):
+def get_preds(arg_dicts,test_ensemble=None):
+    test_ensemble= test_ensemble if(test_ensemble) else ensemble.Ensemble()
+    datasets,n_feats=test_ensemble.get_datasets(**arg_dicts)
+    return test_ensemble.get_prediction(datasets)
+
+def clfs_accuracy(y_true,all_pred):
     return [accuracy_score(y_true,pred_i)
                     for pred_i in all_pred]
 
