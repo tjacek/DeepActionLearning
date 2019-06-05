9343122ef2bb2209a83017b8fb134cff7eeb264d
tjacek <tjacek@student.agh.edu.pl>
2019-06-05 13:07:13 +0200

    ts concat

--- ts/tools.py
+++ ts/tools.py
@@ -14,22 +14,12 @@ def basic_agum(in_path,out_path):
     agum_upsampl=ts.agum.Agumentation([preproc.agum.SamplingAgum()])#,Scale()])
     agum_upsampl(in_path,out_path)
 
-def preproc_concat(in_path,out_path,restr=None,preproc_build=None):
-    paths,norms,preproc_build=prepare_concat(in_path,restr,preproc_build)
-    read_actions=seq.io.build_action_reader(img_seq=False,as_dict=True)
-    action_sets=[read_actions(path_i) for path_i in paths]
-    action_sets=[preproc_build(norm_i)(action_i) 
-                    for action_i,norm_i in zip(action_sets,norms)]
-    concated_actions=seq.tools.concat_actions(action_sets)
-    save_actions=seq.io.ActionWriter(img_seq=False)
+def ts_concat(in_path,out_path,preproc=None):
+    if(type(preproc)==int):
+        preproc=for_ts_network(norm=True,size=preproc)
+    dir_paths=utils.bottom_dirs(in_path)
+    read_actions=seq.io.build_action_reader(img_seq=False,as_dict=True)    
+    all_actions=[ read_actions(path_i) for path_i in dir_paths]
+    concated_actions=seq.tools.concat_actions(all_actions).values()
+    save_actions=seq.io.ActionWriter(False)
     save_actions(concated_actions,out_path)
-
-def prepare_concat(in_path,restr=None,preproc_build=None):
-    paths=utils.top_dirs(in_path)
-    if(type(restr)!=list):
-        restr=[restr]
-    restr=Set(restr)
-    norms=[ not (path_i in restr) for path_i in paths]
-    if(not preproc_build):
-        preproc_build=for_ts_network
-    return paths,norms,preproc_build
